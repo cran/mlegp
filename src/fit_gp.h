8ee734c222cfd5756d9044b51acdcec82434a26c
Garrett M. Dancik <dancikg@nsula.edu>
2008-12-19 00:00:00 +0000

    version 2.2.5

--- src/fit_gp.h
+++ src/fit_gp.h
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    For questions or comments, contact Garrett Dancik (gdancik@iastate.edu)
+    For questions or comments, contact Garrett Dancik (dancikg@nsula.edu)
 ****************************************************************************/
 
 /***************************************************************************
